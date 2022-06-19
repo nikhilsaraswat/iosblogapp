67942ca1103c16b77f6d4ef267606a07a70eb2d5
Nikhil <nikhilsaraswat644@gmail.com>
2022-06-19 16:31:34 +0100

    Initial Commit

---
+++ blogs/blogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogsApp.swift
+//  blogs
+//
+//  Created by Nikhil on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct blogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
